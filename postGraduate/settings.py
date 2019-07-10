f32b8b4fee7842dfcfbb3e35642f3b9c17ed1632
ColorDoge <m18968957166@163.com>
2019-07-11 00:10:14 +0800

    recheck

--- postGraduate/settings.py
+++ postGraduate/settings.py
@@ -96,16 +96,4 @@ HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
 
-PROXY_URL = 'http://47.102.139.163:5010/get/'
 
-MYSQL_HOST = '47.102.139.163'
-
-MYSQL_PORT = 3306
-
-MYSQL_USER = 'root'
-
-MYSQL_PASSWORD = '123456'
-
-MYSQL_DATABASE = 'postGraduate'
-
-SPLASH_URL = 'http://47.102.139.163:8050'
